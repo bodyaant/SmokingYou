f35cfa6e3c7a27fa2104343eef321d36e2babe34
bodyaant <memetosuper@gmail.com>
2026-07-27 19:40:19 +0300

    build: bump version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.smokingtracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10301
-        versionName = "1.3.1"
+        versionCode = 10302
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
