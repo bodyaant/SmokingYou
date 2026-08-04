54e770f7f44dec5c8a1845999570a07df05f82b1
bodyaant <memetosuper@gmail.com>
2026-08-04 22:29:51 +0300

    chore(di): provide BackupManager, AppIconManager, and AchievementsCoordinator

--- app/src/main/java/com/smokingtracker/di/AppModule.kt
+++ app/src/main/java/com/smokingtracker/di/AppModule.kt
@@ -1,18 +1,31 @@
 package com.smokingtracker.di
 
 import androidx.room.Room
+import com.smokingtracker.AchievementsCoordinator
+import com.smokingtracker.AchievementsManager
+import com.smokingtracker.AppIconManager
+import com.smokingtracker.BackupManager
+import com.smokingtracker.HomeViewModel
 import com.smokingtracker.MainViewModel
+import com.smokingtracker.StatisticsManager
 import com.smokingtracker.data.DataStoreManager
 import com.smokingtracker.data.local.SmokingDatabase
 import com.smokingtracker.data.repository.SmokingRepository
 import com.smokingtracker.data.manager.GitHubUpdateManager
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 import com.smokingtracker.data.local.MIGRATION_1_2
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 val appModule = module {
+    single { CoroutineScope(SupervisorJob() + Dispatchers.IO) }
+    single { AchievementsManager() }
+    single { StatisticsManager() }
     single { DataStoreManager(androidContext()) }
     single { GitHubUpdateManager(androidContext()) }
     single {
@@ -20,9 +33,13 @@ val appModule = module {
             androidContext(),
             SmokingDatabase::class.java,
             "smoking_tracker.db"
-        ).addMigrations(MIGRATION_1_2).fallbackToDestructiveMigration().build()
+        ).addMigrations(MIGRATION_1_2).build()
     }
     single { get<SmokingDatabase>().smokingDao() }
-    single { SmokingRepository(get(), get()) }
-    viewModel { MainViewModel(get(), get(), get(), androidContext()) }
+    single { SmokingRepository(get(), get(), get()) }
+    single { AchievementsCoordinator(get(), get(), get(), androidApplication(), get()) }
+    single { AppIconManager(androidApplication()) }
+    single { BackupManager(androidApplication(), get(), get(), get()) }
+    viewModel { MainViewModel(get(), get(), get(), get(), get(), get(), androidApplication()) }
+    viewModel { HomeViewModel(get(), get(), get(), androidApplication()) }
 }
