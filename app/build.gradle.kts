acd1233221fdfed944a2a7b42c43f3c1e1152d60
bodyaant <memetosuper@gmail.com>
2026-08-08 23:53:31 +0300

    chore(gradle): bump versionName to 1.4.1 and versionCode to 10401

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.smokingtracker"
         minSdk = 26
         targetSdk = 37
-        versionCode = 10400
-        versionName = "1.4.0"
+        versionCode = 10401
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
